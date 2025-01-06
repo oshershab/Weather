dc893a9a89657172cf1799d51a0673872107ea66
oshershab <oshershab@gmail.com>
2025-01-06 16:07:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ import streamlit as st
 
 api_key = st.secrets['API_KEY']
 
-def main():
-    api_key = "d3199210c072e0a333eacb3b62351047"
-    city_name = input("Enter a city name: ")
-    print(f"You entered: {city_name}")
-    Weather.get_one_call_weather(city_name, api_key)
-
 
 st.title("My Weather app")
 st.subheader("Enter the name of the city:")
 
+
 user_input = st.text_input("City name:", placeholder="Type city name here")
+
 
 # Button to submit input
 if st.button("Submit"):
