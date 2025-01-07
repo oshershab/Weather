09781682e6e39a376163d1d18c1f717e05b8a22e
oshershab <oshershab@gmail.com>
2025-01-07 16:25:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ st.title("My Weather app")
 st.subheader("Enter the name of the city:")
 
 
-user_input = st.text_input("City name:", placeholder="Type city name here")
+with st.form(key="City name"):
+    user_input = st.text_input("City name:", placeholder="Type city name here")
+    submit_button = st.form_submit_button(label="Submit")
 
 
 # Button to submit input
@@ -19,12 +21,4 @@ if st.button("Submit"):
     else:
         st.warning("Please enter a valid name!")
 
-# Create a form
-with st.form(key="my_form"):
-    user_input = st.text_input("Enter text and press Enter:")
 
-    # Add a submit button
-    submit_button = st.form_submit_button(label="Submit")
-
-if submit_button:
-    st.write(f"You entered: {user_input}")
